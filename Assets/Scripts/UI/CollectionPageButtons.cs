813f2761144e90e1573a15b1f12497604b323b87
Benjamin Uijldert <bmuijldert@gmail.com>
2017-08-10 15:32:17 +0200

    made page flipping system for collection

---
+++ Assets/Scripts/UI/CollectionPageButtons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionPageButtons : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _pages;
+
+    [SerializeField]
+    private int _currentPage = 0;
+
+    private Animator _pageAnimator;
+
+	public void NextPage()
+    {
+        if (_currentPage != _pages.Count - 1)
+        {
+            _pageAnimator = _pages[_currentPage].GetComponent<Animator>();
+            _pageAnimator.SetBool("IsTurningForward", true);
+            _currentPage += 1;
+        }   
+    }
+
+    public void PreviousPage()
+    {
+        if(_currentPage != 0)
+        {
+            _currentPage -= 1;
+            _pageAnimator = _pages[_currentPage].GetComponent<Animator>();
+            _pageAnimator.SetBool("IsTurningForward", false);
+        }
+    }
+}
