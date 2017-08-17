6f3af9bc0a08f576bbd39d3e8846ff9e2ff45a92
Benjamin Uijldert <bmuijldert@gmail.com>
2017-08-17 15:12:50 +0200

    made menu animation and restructured folders

--- Assets/Code/UI/LoadingScreen.cs
+++ Assets/Code/UI/LoadingScreen.cs
@@ -2,29 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadingScreen : MonoBehaviour
 {
-    private float _loadingTime = .5f;
+    private float _loadingTime = .2f;
+
+    [SerializeField]
+    private Sprite loadingImage;
 
     public void LoadLevel(string sceneName)
     {
-        StartCoroutine(LoadTheLevel(sceneName));
+        StartCoroutine(LoadTheLevel(sceneName, 0f, 90f));
     }
 
-    private IEnumerator LoadTheLevel(string sceneName)
+    private IEnumerator LoadTheLevel(string sceneName, float begin, float end)
     {
         float elapsedTime = 0f;
-        float yRotation = 90f;
+        float yRotation = begin;
 
         while (elapsedTime < _loadingTime)
         {
-            yRotation = Mathf.Lerp(90f, 0f, (elapsedTime / _loadingTime));
+            yRotation = Mathf.Lerp(begin, end, (elapsedTime / _loadingTime));
             transform.eulerAngles = new Vector3(0f, yRotation, 0f);
             elapsedTime += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
-        transform.eulerAngles = new Vector3(0f, 90f, 0f);
-        SceneManager.LoadScene(sceneName);
+        transform.eulerAngles = new Vector3(0f, end, 0f);
+        if(end == 90f)
+        {
+            StartCoroutine(LoadTheLevel(sceneName, 90f, 0f));
+            GetComponent<Image>().sprite = loadingImage;
+        }
+        else
+        {
+            yield return new WaitForSeconds(.25f);
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
---
+++ Assets/Code/Utility/RotateTransform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTransform : MonoBehaviour
+{
+    //[SerializeField]
+    private float _rotateTime = 0.25f;
+    [SerializeField]
+    private float _beginRotation;
+    [SerializeField]
+    private float _endRotation;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(LerpRotation());
+    }
+
+    private IEnumerator LerpRotation()
+    {
+        float elapsedTime = 0;
+        float yRotation = _beginRotation;
+
+        while (elapsedTime < _rotateTime)
+        {
+            yRotation = Mathf.Lerp(_beginRotation, _endRotation, (elapsedTime / _rotateTime));
+            transform.eulerAngles = new Vector3(0f, yRotation, 0f);
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        transform.eulerAngles = new Vector3(0f, _endRotation, 0f);
+    }
+}
