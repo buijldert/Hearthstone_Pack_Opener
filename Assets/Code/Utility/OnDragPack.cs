76be8a994abb2b9f7ceaa4a60a6fbb6278f33d01
Benjamin Uijldert <bmuijldert@gmail.com>
2017-08-29 15:17:49 +0200

    added several visual effects

--- Assets/Code/Utility/OnDragPack.cs
+++ Assets/Code/Utility/OnDragPack.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class OnDragPack : MonoBehaviour
 {
@@ -16,12 +17,12 @@ public class OnDragPack : MonoBehaviour
 
     private bool _isOpeningPack = false;
 
-    [SerializeField]
-    private GameObject _packOpenExplosion;
+    private ParticleSystemRenderer[] _dragParticles;
 
     void Start()
     {
         _packReceiver = GameObject.Find("PackReceiver").transform;
+        _dragParticles = GameObject.Find("FlowContainer").GetComponentsInChildren<ParticleSystemRenderer>();
     }
 
     private void Update() 
@@ -35,6 +36,7 @@ public class OnDragPack : MonoBehaviour
             else
             {
                 transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100f));
+                ChangeFlowVisibility(0);
             }
         }
     }
@@ -43,6 +45,7 @@ public class OnDragPack : MonoBehaviour
     {
         if(_isOpeningPack == false)
         {
+            ChangeFlowVisibility(-1);
             _distance = Vector2.Distance(transform.position, _packReceiver.position);
             if (_distance < _maxDistance)
             {
@@ -55,22 +58,42 @@ public class OnDragPack : MonoBehaviour
             {
                 _onDragBeginPack.AddPack();
                 Destroy(gameObject);
-                ///ReturnToBeginPosition();
             }
         } 
     }
 
     private IEnumerator OpenPackDelay()
     {
-        while (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("openPack"))
-        {
-            yield return new WaitForEndOfFrame();
-        }
+        yield return new WaitForSeconds(0.5f);
+
+        ParticleSystemRenderer packGlow = GameObject.Find("Cell_02").GetComponent<ParticleSystemRenderer>();
+        ParticleSystem packGlowParticleSystem = packGlow.gameObject.GetComponent<ParticleSystem>();
+        
+        packGlow.sortingOrder = 0;
+        packGlowParticleSystem.Play();
+
+        yield return new WaitForSeconds(0.5f);
+
+        packGlow.sortingOrder = -1;
+        packGlowParticleSystem.Pause();
+
         if (OnOpenPack != null)
         {
             OnOpenPack(_packExpansion);
-            Instantiate(_packOpenExplosion, transform.position, Quaternion.identity);
+
+            ForkParticleEffect explosionEffect = GameObject.Find("Stylized_Space_Explosion").GetComponent<ForkParticleEffect>();
+            explosionEffect.RestartEffect();
+            explosionEffect.PlayEffect();
+
             Destroy(gameObject);
+        }
+    }
+
+    private void ChangeFlowVisibility(int sortingOrder)
+    {
+        for (int i = 0; i < _dragParticles.Length; i++)
+        {
+            _dragParticles[i].sortingOrder = sortingOrder;
         }
     }
 }
